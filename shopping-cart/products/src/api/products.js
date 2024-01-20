077f615a422655fa455dc7c1e3af6e94e99c954a
Olakunle Boye <kunlexzy@gmail.com>
2024-01-20 18:46:36 +0100

    add service product

--- shopping-cart/products/src/api/products.js
+++ shopping-cart/products/src/api/products.js
@@ -1,5 +1,14 @@
+const ProductsService = require("../services/product.service");
+
 module.exports = (app) => {
-  app.get('/', (req, res, next) => {
-    res.send('This is from product');
+  const service = new ProductsService();
+
+  app.get("/", async (req, res, next) => {
+    try {
+      const data = await service.GetProducts();
+      return res.status(200).json(data);
+    } catch (error) {
+      return res.status(404).json({ error });
+    }
   });
 }
---
+++ shopping-cart/products/src/services/product.service.js
@@ -0,0 +1,7 @@
+class ProductService {
+  async GetProducts() {
+    return 'This is from product service';
+  }
+}
+
+module.exports = ProductService;
